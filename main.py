86ca2b8bf02a975f1daacd3b23f88b0c38be896a
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-06-01 22:50:18 -0400

    if statements for comp

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ rules = [r,s,p]
 
 computer = random.randint(0,2)
 
+print(computer)
+
+comp_move = s 
+
+if computer == 0:
+    comp_move  = r
+
+if computer == 1:
+    comp_move = p
+
+print(computer)
+
+
+
+ 
+
