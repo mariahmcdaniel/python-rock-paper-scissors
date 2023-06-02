4c3d86ccaf027d7961543475f9a6751d51644b85
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-06-01 22:43:37 -0400

    random imported, computer defined

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-player = input("Rock, Paper, or Scissors?: ")
+import random
+
+player = input("                  READY?\n\n\n                    Enter 'r' for rock, 'p' for paper, or 's' for scissors: ")
 r = ["s", "s", "r"]
 p = ["r", "r", "p"]
 s = ["p", "p", "s"]
 rules = [r,s,p]
+
+computer = random.randint(0,2)
 
