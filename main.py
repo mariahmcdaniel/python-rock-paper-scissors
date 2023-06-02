a4498f91c6f7a3d8f95aa934f98cf36a3aea67fe
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-06-01 23:06:12 -0400

    added art

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
 import random
 
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+
 player = input("                  READY?\n\n\n                    Enter 'r' for rock, 'p' for paper, or 's' for scissors: ")
 r = ["s", "s", "r"]
 p = ["r", "r", "p"]
 s = ["p", "p", "s"]
 rules = [r,s,p]
+
+if player.lower() == "r":
+    print(rock)
+elif player.lower() == "p":
+    print(paper)
+elif player.lower() == "s":
+    print(scissors)        
 
 computer = random.randint(0,2)
 
@@ -14,11 +49,17 @@ comp_move = s
 
 if computer == 0:
     comp_move  = r
+    print(f"    COMPUTER CHOSE:\n                                 {rock}")
 
 elif computer == 1:
     comp_move = p
+    print(f"    COMPUTER CHOSE:\n                                 {paper}")
+
+else:
+    print(f"    COMPUTER CHOSE:\n                                 {scissors}")
 
 print(computer)
+print(comp_move)
 
 
 result = comp_move.count(player)
